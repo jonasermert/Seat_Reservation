ef31c7ae60d1b79754ba6e4236ba1a61302bcd39
Jonas <ermert.jonas@yahoo.de>
2022-07-23 13:03:21 +0200

    Initial commit

---
+++ src/de/jonasermert/Main.java
@@ -0,0 +1,19 @@
+package de.jonasermert;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Theatre theatre = new Theatre("Erfurt", 8, 12);
+//        theatre.getSeats();
+        if(theatre.reserveSeat("H11")) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
+        if(theatre.reserveSeat("H11")) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
+    }
+}
